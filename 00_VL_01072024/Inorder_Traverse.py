fe6fba85ebb12a5f4f527116a6d746ca88ce8627
jMarlonLeon <marlon_leon.juntorius@fom-net.de>
2024-07-01 11:12:38 +0200

    Inorder_Traverse

---
+++ 00_VL_01072024/Inorder_Traverse.py
@@ -0,0 +1,23 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Was ist diese Klasse ? : Diese Klasse ist ein Knoten in einem binären Baum
+
+def meinefunktion(node):
+    if node:
+        meinefunktion(node.left)
+        print(node.data, end=" ")
+        meinefunktion(node.right)
+
+# Was macht meinefunktion() ?
+# meinefunktion = Inorder Traversal
+
+
+# Was ist das ? : Ein gerichteter, binärer Baum
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
