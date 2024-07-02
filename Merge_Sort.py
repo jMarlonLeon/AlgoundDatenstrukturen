33a738a604167f408b18aab5186e966a251d9127
jMarlonLeon <marlon_leon.juntorius@fom-net.de>
2024-07-02 13:57:36 +0200

    Merge_Sort

---
+++ Merge_Sort.py
@@ -0,0 +1,43 @@
+def merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) // 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    
+    # Erstelle Hilfsarrays L und R
+    L = [0] * (n1 + 1)
+    R = [0] * (n2 + 1)
+    
+    for i in range(n1):
+        L[i] = A[p + i]
+    
+    for j in range(n2):
+        R[j] = A[q + j + 1]
+    
+    # Setze Wächterelemente
+    L[n1] = float('inf')
+    R[n2] = float('inf')
+    
+    i = 0
+    j = 0
+    
+    for k in range(p, r + 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+
+start = [3,6,1,7,5,2,9,8]
+eingabe = start.copy()
+
+print("Unsortiertes Array:", start)
+merge_sort(eingabe, 0, len(eingabe) - 1)
+print("Sortiertes Array:", eingabe)
