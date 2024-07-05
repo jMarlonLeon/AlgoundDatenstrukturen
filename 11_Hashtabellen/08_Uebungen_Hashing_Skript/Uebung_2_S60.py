db2fe755af5c0e5f49a751658420e8331cfe42c5
jMarlonLeon <marlon_leon.juntorius@fom-net.de>
2024-07-05 13:03:41 +0200

    Uebung_Hashing_Skript_3

---
+++ 11_Hashtabellen/08_Uebungen_Hashing_Skript/Uebung_2_S60.py
@@ -0,0 +1,33 @@
+from math import sqrt, floor
+
+GROESSE = 1000
+
+hashtabelle = [None] * GROESSE
+
+def hashfunktion(schluessel):
+    A = (sqrt(5) -1) /2
+    # alternative zu floor geht auch int() oder // 1
+    hashwert = floor(GROESSE * (schluessel * A % 1))
+    return hashwert
+
+def einfuegen(schluessel):
+    index = hashfunktion(schluessel)
+    hashtabelle[index] = schluessel
+
+def suchen(schluessel):
+    index = hashfunktion(schluessel)
+    return hashtabelle[index]
+
+def ausgeben():
+    for Slot, wert in enumerate(hashtabelle):
+        if wert is not None:
+            print(f"Slot {Slot}: {wert}")
+
+einfuegewerte = [61,62,63,64,65]
+
+for wert in einfuegewerte:
+    einfuegen(wert)
+
+ausgeben()
+
+        
